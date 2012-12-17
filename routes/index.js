e188e8fd1f97da56e9e22bb29d7a174b65f42bfe
Dan McAulay <dmcaulay@gmail.com>
2012-12-16 17:43:28 -0800

    fixing the app

--- routes/index.js
+++ routes/index.js
@@ -13,27 +13,27 @@ function error(res, err) {
   res.end();
 }
 
-function render(layout, body, res) {
+function render(res, layout, body) {
   read(layout, function(err, layout) {
     if (err) return error(res, err);
     if (!body) {
       res.writeHead(200, {'Content-Type': mime.lookup('html')});
-      res.end(layout);
+      return res.end(layout);
     }
     read(body, function(err, body) {
-      if (err) return error(self.res, err);
-      self.res.writeHead(200, {'Content-Type': mime.lookup('html')});
-      self.res.end(Plates.bind(layout, {body: body}));
+      if (err) return error(res, err);
+      res.writeHead(200, {'Content-Type': mime.lookup('html')});
+      res.end(Plates.bind(layout, {body: body}));
     });
   });
 }
 
 module.exports = function(router) {
   router.get('/', function() {
-    render(res, 'layout');
+    render(this.res, 'layout');
   });
 
   router.get('/login', function() {
-    render(res, 'layout', 'login');
+    render(this.res, 'layout', 'login');
   });
 };
